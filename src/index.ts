f4c779aa8fe7300142c0cfa231be71a27d8f7cf5
Uliana Nadorozhna <unadorozhna1@gmail.com>
2024-01-02 14:49:09 +0200

    first

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { concatenation } from "./concatonation";
+
+const button = document.querySelector('button')!;
+const input = document.querySelector('input')!;
+
+if (button && input) {
+  button.addEventListener('click', () => {
+    concatenation(input.value, 'hello!');
+  });
+}
